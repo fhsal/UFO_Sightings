fb2ff00d6da8405e72f21b5cd6a04d80a4a75231
F.  Salvo <68975104+fhsal@users.noreply.github.com>
2022-08-04 09:23:53 -0400

    updating listener connecting it to new function to update table

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 // import the data from data.js
 const tableData = data;
-
+var workingTable = tableData
 
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
@@ -44,8 +44,41 @@ function handleClick() {
   buildTable(filteredData);
 }
 
+function sayHello(){console.log('Hello')}
+
+
+function updateTable(){
+
+  filtered_inputs = []
+
+  inputs = {"state": d3.select("#state").property("value"),
+  "country": d3.select("#country").property("value"),
+  "shape": d3.select("#shape").property("value"),
+  "datetime": d3.select("#datetime").property("value")}
+
+  Object.entries(inputs).forEach(item => {
+    if (item[1])
+      {filtered_inputs.push(item)}})
+
+    // filter the tableData based upon each filter entered 
+  filtered_inputs.forEach((input) => {
+    let inputField = input[0]
+    let userInput = input[1]
+    console.log("field = " + inputField);
+    console.log("userInput = " + userInput);
+      workingTable = workingTable.filter(row => row[inputField] === userInput);
+  
+    });
+
+  // rebulid table display 
+
+  buildTable(workingTable)
+  
+
+}
+
 // Attach an event to listen for the form button
-d3.selectAll("#filter-btn").on("click", handleClick);
+d3.selectAll("input").on("change", updateTable);
 
 // Build the table when the page loads
 buildTable(tableData);
